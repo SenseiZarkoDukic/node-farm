85d83d29e82c6bea0e317e1e375b642c1c3abaea
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-02-23 18:47:12 +0100

    Refactor file reading to use non-blocking, asynchronous approach

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf8");
-console.log(textIn);
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf8");
+// console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written!");
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+    });
+  });
+});
+
+console.log("Will read file!");
