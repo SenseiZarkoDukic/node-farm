b3d184210e658bb31ddb870bd12da518857a6745
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-02-24 21:26:14 +0100

    Add http module and comment out file reading code

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+
+///////////////////////////////////////////////
+// FILES
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf8");
@@ -9,18 +13,21 @@ const fs = require("fs");
 // console.log("File written!");
 
 // Non-blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("ERROR! 💥");
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log(data3);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR! 💥");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written! 🤞");
-      });
-    });
-  });
-});
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written! 🤞");
+//       });
+//     });
+//   });
+// });
 
-console.log("Will read file!");
+// console.log("Will read file!");
+
+///////////////////////////////////////////////
+// SERVER
