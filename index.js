d2ce182ab43319bc9b9dd1eb1b4f2e72588becb6
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-02-27 02:13:21 +0100

    Add console.log for slugify function

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ const tempCard = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+console.log(slugify("Fresh Avocados", { lower: true }));
+
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
