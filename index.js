0a953359fea425c2bc67cbd52c0f575d6fe358eb
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-02-27 02:17:37 +0100

    Add slugify functionality to index.js

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ const tempCard = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-console.log(slugify("Fresh Avocados", { lower: true }));
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
